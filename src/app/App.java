c8cb0561de711f4ea1545d9b860cea9a692e4ca0
prettydy <prettydt@163.com>
2019-10-21 11:06:47 +0800

    wq

--- src/app/App.java
+++ src/app/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
         System.out.println("fafafa");
+        System.out.println("second commit");
     }
 }
